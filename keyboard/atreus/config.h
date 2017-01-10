ebebf50e80c5c82b63e90e03505093ecbb310837
Tommy Lincoln <pajamapants3000@gmail.com>
2017-01-10 04:46:04 -0800

    Update 2017-01-10

--- keyboard/atreus/config.h
+++ keyboard/atreus/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_HAS_GHOST
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE    5
+#define DEBOUNCE    0
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 // #define LOCKING_SUPPORT_ENABLE
@@ -56,19 +56,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* disable debug print */
 #ifndef NO_DEBUG
-//#define NO_DEBUG
+#define NO_DEBUG
 #endif
 
 /* disable print */
 #ifndef NO_DEBUG
-//#define NO_PRINT
+#define NO_PRINT
 #endif
 
 /* disable action features */
 //#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
 #endif
